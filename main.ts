ab9857a8c2b3a7578bbd1933fa405e94a864f6fa
phoenixvane <32545460+phoenixvane@users.noreply.github.com>
2021-04-29 10:30:47 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.A, function () {
+    Start = 1
+})
+input.onButtonPressed(Button.B, function () {
+    Start = 0
+})
+let Start = 0
+Start = 0
+basic.forever(function () {
+    basic.showNumber(led.brightness())
+    if (Start && led.brightness() > 34) {
+        music.playMelody("D E D E C5 B - G ", 170)
+    }
+})
